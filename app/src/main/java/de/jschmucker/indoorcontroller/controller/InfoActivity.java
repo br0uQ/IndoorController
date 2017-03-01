4e0cd6d45f8a9559820800834e4e8eef87298ea7
br0uQ <joshua.schmucker@gmail.com>
2017-03-01 09:03:54 +0100

    Added Infos to InfoActivity (version, contact info, link to the sourcecode)
    Working:
    Add task, Change task, Delete Task, Save and load task
    Add action, Delete Action, Save and load Action
    Add location, Change location, Delete Location, Save and load location
    RoomDetection, NFC Detection, WifiDetection

--- app/src/main/java/de/jschmucker/indoorcontroller/controller/InfoActivity.java
+++ app/src/main/java/de/jschmucker/indoorcontroller/controller/InfoActivity.java
@@ -1,9 +1,12 @@
 package de.jschmucker.indoorcontroller.controller;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import de.jschmucker.indoorcontroller.BuildConfig;
 import de.jschmucker.indoorcontroller.R;
 
 public class InfoActivity extends AppCompatActivity {
@@ -13,6 +16,12 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         setTitle(R.string.about);
+
+        String versionName = BuildConfig.VERSION_NAME;
+
+        TextView version = (TextView) findViewById(R.id.info_textView_version);
+        version.setText("Version: " + versionName);
+
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
